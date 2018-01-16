b543204228e8a00306b4adcd725184b71e0b6f95
Anna Carollyne Dantas de Lima <201613530029@ad.ifpb.edu.br>
2018-01-16 15:23:18 -0300

    reenviando

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 const PORT = process.env.PORT || 5000
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(PORT, function () {
-  console.log('Example app listening on port: ' + PORT);
-});
+app.listen(PORT, () => console.log('Example app running!'))
